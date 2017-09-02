2998e5704fa562b0f0b2ad85a5ddd31a29a847a4
Tim Turner <timmyturner@me.com>
2017-09-01 21:03:23 -0700

    fixing tests and stuff

--- lab-tim/__test__/sll.test.js
+++ lab-tim/__test__/sll.test.js
@@ -5,7 +5,7 @@ require('jest');
 
 describe('Testing Singly Linked List', function() {
   describe('default properties', () => {
-    beforeAll(done => {
+    beforeEach(done => {
       this.sll = new SLL();
       done();
     });
@@ -20,7 +20,7 @@ describe('Testing Singly Linked List', function() {
       done();
     });
     describe('add a new node to the head of the list', () => {
-      test('should add a new node to Head', () => {
+      test('should add a new node to list', () => {
         this.sll.prepend(2);
         expect(this.sll.head.val).toBe(2);
         expect(this.sll.head.next).toBeNull();
@@ -31,6 +31,13 @@ describe('Testing Singly Linked List', function() {
         expect(this.sll.head.next.val).toBe(2);
         expect(this.sll.head.next.next).toBeNull();
       });
+      test('should add a third node to the list', () => {
+        this.sll.prepend(4);
+        expect(this.sll.head.val).toBe(4);
+        expect(this.sll.head.next.val).toBe(3);
+        expect(this.sll.head.next.next.val).toBe(2);
+        expect(this.sll.head.next.next.next).toBeNull();
+      });
     });
   });
   describe('#append', () => {
@@ -38,27 +45,53 @@ describe('Testing Singly Linked List', function() {
       this.sll = new SLL();
       done();
     });
-    test('add a node to the tail of the list', () => {
-      this.sll.append(4);
-      this.sll.append(5);
-      this.sll.append(6);
-      expect(this.sll.head.next.next.val).toBe(6);
+    describe('add a new node to the tail of the list', () => {
+      test('should add a new node to list', () => {
+        this.sll.append(2);
+        expect(this.sll.head.val).toBe(2);
+        expect(this.sll.head.next).toBeNull();
+      });
+      test('should add a second node to the list', () => {
+        this.sll.append(3);
+        expect(this.sll.head.val).toBe(2);
+        expect(this.sll.head.next.val).toBe(3);
+        expect(this.sll.head.next.next).toBeNull();
+      });
+      test('should add a third node to the list', () => {
+        this.sll.append(4);
+        expect(this.sll.head.val).toBe(2);
+        expect(this.sll.head.next.val).toBe(3);
+        expect(this.sll.head.next.next.val).toBe(4);
+        expect(this.sll.head.next.next.next).toBeNull();
+      });
     });
   });
 
   describe('#reverse', () => {
-    beforeAll(done => {
+    beforeEach(done => {
       this.sll = new SLL();
       done();
     });
-    test('should reverse the list', () => {
-      this.sll.append(2);
-      this.sll.append(4);
-      this.sll.append(6);
-      this.sll.reverse();
-      expect(this.sll.head.val).toBe(6);
-      expect(this.sll.head.next.val).toBe(4);
-      expect(this.sll.head.next.next.val).toBe(2);
+    describe('should reverse the list given valid input', () => {
+      test('should reverse the list', () => {
+        this.sll.append(2);
+        this.sll.append(4);
+        this.sll.append(6);
+        this.sll.reverse();
+        expect(this.sll.head.val).toBe(6);
+        expect(this.sll.head.next.val).toBe(4);
+        expect(this.sll.head.next.next.val).toBe(2);
+      });
+      test('should return null because the list is empty', () => {
+        this.sll.reverse();
+        console.log(this.sll.head);
+        expect(this.sll.head).toBeNull();
+      });
+      test('should return the list unaltered because it only has one node', () => {
+        this.sll.append(5);
+        this.sll.reverse();
+        expect(this.sll.head.val).toBe(5);
+      });
     });
   });
 });
