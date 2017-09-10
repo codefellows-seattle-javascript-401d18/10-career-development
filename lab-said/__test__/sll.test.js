4e37fe572ce140a2ebe4b86a33ab2872b40bf846
saidmattar <ssabdou@gmail.com>
2017-09-09 22:55:08 -0700

    added two reverse method tests

--- lab-said/__test__/sll.test.js
+++ lab-said/__test__/sll.test.js
@@ -71,4 +71,38 @@ describe('Testing Singly Linked List', function() {
       expect(this.sll.head.val).toBe('cows');
     });
   });
+
+  describe('#reverse', () => {
+    beforeAll(done => {
+      this.sll = new SLL();
+      done();
+    });
+    test('reverse the order of strings', () => {
+      this.sll.append('cows');
+      this.sll.append('keys');
+      this.sll.append('towers');
+      this.sll.reverse();
+      expect(this.sll.head.val).toBe('towers');
+      expect(this.sll.head.next.val).toBe('keys');
+    });
+  });
+  describe('#reverse', () => {
+    beforeAll(done => {
+      this.sll = new SLL();
+      done();
+    });
+    test('reverse the order with lots', () => {
+      this.sll.append(4);
+      this.sll.append(5);
+      this.sll.append(6);
+      this.sll.append(7);
+      this.sll.append(8);
+      this.sll.append(9);
+      this.sll.reverse();
+      expect(this.sll.head.val).toBe(9);
+      expect(this.sll.head.next.val).toBe(8);
+      expect(this.sll.head.next.next.val).toBe(7);
+      expect(this.sll.head.next.next.next.val).toBe(6);
+    });
+  });
 });
